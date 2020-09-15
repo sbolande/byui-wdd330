d2afe731c63861655469a5930cb9181b192decaa
Seth Bolander <bol15015@byui.edu>
2020-09-14 19:34:35 -0600

    began week01 notes

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const links = [
+    {
+        label: "Week01 Notes",
+        url: "week01/index.html"
+    }
+];
+
+links.forEach(link => {
+    document
+        .getElementById("#links")
+        .append(`<li><a href="${link.url}">${link.label}</a></li>`);
+});
