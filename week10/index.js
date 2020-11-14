0790a0d13ab3fb436078435221c9ee473756a7b5
Seth Bolander <bol15015@byui.edu>
2020-11-14 14:28:12 -0700

    edits

--- week10/index.js
+++ week10/index.js
@@ -1,4 +1,4 @@
 import { getJSON, getLocation } from './utilities.js';
 import QuakesController from './QuakesController.js';
 
-new QuakesController().init();
+new QuakesController(document.getElementById('quakeList')).init();
